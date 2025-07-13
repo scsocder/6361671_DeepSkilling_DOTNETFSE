56d3a190f894ceeb0e0d646f0cffd9f9ec38214c
scsocder <shubamchakraborty1997@gmail.com>
2025-07-14 01:19:43 +0530

    Add files via upload
    WEbapi 3 handson

---
+++ CompanyWebAPI/CompanyWebAPI/Filters/CustomExceptionFilter.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CompanyWebAPI.Filters
+{
+    public class CustomExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            var exceptionDetails = context.Exception.ToString();
+            File.WriteAllText("ExceptionLogs.txt", exceptionDetails);
+
+            context.Result = new ObjectResult("An error occurred in the application.")
+            {
+                StatusCode = StatusCodes.Status500InternalServerError
+            };
+        }
+    }
+}
